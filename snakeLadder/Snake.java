3f70dd8ca189cdf99265522edca076ff3caefacf
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-10 00:03:28 +0530

    uc3 pushed

--- snakeLadder/Snake.java
+++ snakeLadder/Snake.java
@@ -16,6 +16,21 @@ while(pos!=100)
 			{
 				dice = (int) (Math.random()*(max-min+1)+min);
 				System.out.println("Dice number is " + dice);
+				i++;
+				int  choice = (int) (Math.random()*(mx-mn+1)+mn);
+				System.out.println("Choice is " + choice );
+				if(choice == 0)
+				{
+					pos=pos+0;
+				}
+				if(choice == 1)
+				{
+					pos=pos+dice;
+				}
+				if(choice == 2)
+				{
+					pos=pos- dice;
+				}
 				
 			
 	}
