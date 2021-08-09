dc5fe55422fbd9224eec03bb83e27163d82e73cf
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-10 00:07:10 +0530

    uc6 pushed

--- snakeLadder/Snake.java
+++ snakeLadder/Snake.java
@@ -7,7 +7,7 @@ package com.Bridgelabz.snakeLadder;
 public class Snake {
 	public static int pos = 0;
 	public static int dice = 0;
-	public static int w=0;
+
 
 	public static void main(String[] args) {
 		int min=0, max=6,mn=0,mx=2;
